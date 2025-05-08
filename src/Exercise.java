987718ecc7a6577f3306bfed19db80e09fe2ff86
Ponomarenko Roman <165600973+K08080KK@users.noreply.github.com>
2025-05-08 11:01:06 +0000

    ready

--- src/Exercise.java
+++ src/Exercise.java
@@ -1,7 +1,33 @@
 //package domain;
 
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+    public static String Calculate(int cidr){
+        if(cidr<0||cidr>32){
+            return "Invalid CIDR";
+        }
+
+        int mask = 0xFFFFFFFF << (32 - cidr);
+        int network = 0;
+        int broadcast = network | ~mask;
+
+        String subnetMask = String.format("%d.%d.%d.%d",
+            (mask>>>24)&0XFF,
+            (mask>>>16)&0XFF,
+            (mask>>>8)&0XFF,
+            mask & 0xFF);
+
+        String rangeStart = String.format("%d.%d.%d.%d",
+            (network>>>24)&0XFF,
+            (network>>>16)&0XFF,
+            (network>>>8)&0XFF,
+            network & 0xFF);
+        
+        String rangeEnd = String.format("%d.%d.%d.%d",
+            (broadcast>>>24)&0XFF,
+            (broadcast>>>16)&0XFF,
+            (broadcast>>>8)&0XFF,
+            broadcast & 0xFF);
+        
+        return "Mask: " + subnetMask + "\nRange: " + rangeStart + " - " + rangeEnd;
     }
 }
